a427b2b56d6c3d2bb2e3afbc0080ca759ea19e9e
sabeel khan <sabeelkhan9621@gmail.com>
2024-04-26 22:18:19 +0530

    conditional

---
+++ java-script/conditional-opraters.js
@@ -0,0 +1,224 @@
+// let a = 5;
+// let b = 8;
+// if (a > b) {
+//     console.log("a is max")
+// }
+// else {
+//     console.log("b is max")
+// }
+
+//  four number maximum
+
+// let a = 10;
+// let b = 20;
+// let c = 50;
+// let d = 80;
+// if (a > b) {
+//     if (a > c) {
+//         if (a > d) {
+//           console.log(a)
+//         }
+//         else {
+//             console.log(d)
+//         }
+//     }
+// }
+// if (b > c) {
+//     if (b > d) {
+//         console.log(b)
+
+//     }
+//     else {
+//         console.log(d)
+//     }
+// }
+// if (c > a) {
+//     if (c > d) {
+//      console.log(c)
+//     }
+//     else {
+//         console.log(d)
+//     }
+// }
+
+// right a progrma to check whether a number positive negetive or zero
+
+
+
+// let a = 0;
+// if (a < 0) {
+//     console.log("nigative")
+// }
+// else if (a > 0) {
+// console.log("positive")
+// }
+// else {
+//     console.log("zero")
+// }
+
+
+
+// write a java script program to check a number divisible by 5 or 11
+
+
+// let a = 50;
+// if (a%5==0 && a%11==0) {
+//     console.log("divisble")
+// }
+// else {
+//     console.log("not")
+// }
+
+// write a java script program to check whetger a number is even or odd
+
+
+// let a = 55;
+// if (a % 2 == 0) {
+//     console.log("even number")
+// }
+// else {
+//     console.log ("odd number")
+// }
+
+
+// leap year or not
+
+// let a = 2004;
+// if (a%400== 0 || a%4== 0) {
+//     console.log("leap year")
+// }
+// else if (a%100== 0) {
+//     console.log("not")
+// }
+// else {
+//     console.log("not")
+// }
+
+
+// write a java script program to check  alphabet or not
+
+// let a = '10'
+// if (a >= 0) {
+//     console.log("number");
+// } else if
+//     (a >= "a" && a <= "z") {
+//     console.log("alphabet");
+// }
+// else {
+//     console.log("special");
+//     }
+
+
+
+// write a java script program to input any alphabet and check whether it is wovel or consonent
+
+
+// let a='h';
+// if (a == 'a'||a == 'o'||a == 'i'||a == 'e'||a == 'u') {
+//     console.log("wovel")
+// }
+// else {
+//     console.log("consonent")
+// }
+
+//   write a java script program to input any charecter and check wheher it is alphabet digit or special charector
+
+
+// let a = '10';
+// if (a >= 'a'&& a<='z'){
+//     console.log("chrecter")
+
+// }
+// else if (a >= '0') {
+//     console.log("number")
+// }
+// else {
+//     console.log("special charecter")
+// }
+
+
+
+// write a java script program to check whether a charecter is upper case and lower case
+
+// let a = 'a'
+// if (a >= 'a' && a <= 'z') {
+//     console.log("lowercase")
+// }
+// else {
+//     console.log("upper case")
+// }
+
+
+// write a java script program to input week number and print week day
+
+// let a = 7;
+// if (a==1) {
+//     console.log("monday")
+// }
+// else if (a==2) {
+//     console.log("tuesday")
+// }
+// else if (a==3) {
+//     console.log("wednesday")
+// }
+// else if (a==4) {
+//     console.log("thursday")
+// }
+// else if (a==5) {
+//     console.log("friday")
+// }
+// else if (a==6) {
+//     console.log("saturday")
+// }
+// else if (a==7) {
+//     console.log("sunaday")
+// }
+// else {
+//     console .log ("mismatch ")
+// }
+
+
+
+// month count
+
+
+// let a = 1;
+// if (a==1) {
+//     console.log("jan")
+// }
+// else if (a==2) {
+//     console.log("feb")
+// }
+// else if (a==3) {
+//     console.log("march")
+// }
+// else if (a==4) {
+//     console.log("april")
+// }
+// else if (a==5) {
+//     console.log("may")
+// }
+// else if (a==6) {
+//     console.log("june")
+// }
+// else if (a==7) {
+//     console.log("july")
+// }
+// else if(a==8) {
+//     console.log ("aug")
+// }
+// else if (a==9) {
+//     console.log("sep")
+// }
+// else if (a==10) {
+// console.log("oct")
+// }
+// else if (a==11) {
+//     console.log(nov)
+// }
+// else if (a==12) {
+// console.log("dec")
+// }
+// else {
+//     console.log("input mismatch")
+// }
