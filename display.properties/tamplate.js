a548795cbe1ed46515640c8ba3b4fe749dd108b9
sabeel khan <sabeelkhan9621@gmail.com>
2024-06-12 23:34:38 +0530

    tamplate with_css_and_javascript

---
+++ display.properties/tamplate.js
@@ -0,0 +1,26 @@
+let slideIndex = 0;
+showSlides();
+
+// Next/previous controls
+function plusSlides (n) {
+    showSlides(slideIndex += n);
+}
+
+// Show slides
+function showSlides (n) {
+    let slides = document.getElementsByClassName("slides");
+    if (n === undefined) {
+        slideIndex++;
+    } else {
+        slideIndex = n;
+    }
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    if (slideIndex < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+    if (n === undefined) {
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+}
