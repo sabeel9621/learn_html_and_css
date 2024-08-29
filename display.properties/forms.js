54327040ed0cc235e85ace01c1cf76cb14d13fb6
sabeel khan <sabeelkhan9621@gmail.com>
2024-08-29 11:24:23 +0530

    colorlib

--- display.properties/forms.js
+++ display.properties/forms.js
@@ -24,27 +24,3 @@ signupbtn.addEventListener('click', () => {
 })
 
 
-// let signUpBtn = document.querySelector('.signupbtn');
-// let signInBtn = document.querySelector('.singinbtn');
-// let nameFeild = document.querySelector('.namefeild');
-// let title = document.querySelector('.title');
-// let underline = document.querySelector('.underline');
-// let text = document.querySelector('.text');
-
-// signInBtn.addEventListener('click', () => {
-//     nameFeild.style.maxHeight = '0';
-//     title.innerHTML = 'sign In';
-//     text.innerHTML = 'lost password';
-//     signUpBtn.classList.add('disiable');
-//     signInBtn.classList.remove('disiable');
-//     underline.style.transform = 'translateX(35px)';
-// });
-
-// signUpBtn.addEventListener('click', () => {
-//     nameFeild.style.maxHeight = '60px';
-//     title.innerHTML = 'sign Up';
-//     text.innerHTML = 'password saggestion';
-//     signUpBtn.classList.remove('disiable');
-//     signInBtn.classList.add('disiable');
-//     underline.style.transform = 'translateX(0)';
-// });
