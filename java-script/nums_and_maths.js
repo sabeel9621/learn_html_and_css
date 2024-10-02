7c1f78835db64dbd8047dc66cd69aa0da5ef4c85
sabeel khan <sabeelkhan9621@gmail.com>
2024-10-02 22:21:16 +0530

    number-And-maths

---
+++ java-script/nums_and_maths.js
@@ -0,0 +1,14 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100)
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+
+const othernumber = 123.8966
+console.log(othernumber.toPrecision(4));
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-In'))
--- java-script/someString-Mathodes.js
+++ java-script/someString-Mathodes.js
@@ -82,3 +82,4 @@ console.log(GameName.toString());
 
 
 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
