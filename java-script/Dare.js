ae9b7f8a0498eec35ccfbfebd849ec0843b85bc6
sabeel khan <sabeelkhan9621@gmail.com>
2024-10-05 22:45:47 +0530

    date_and_time

--- java-script/Dare.js
+++ java-script/Dare.js
@@ -1,3 +1,31 @@
+// // Dates
 let myDate = new Date()
 console.log(myDate);
 console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+
+
+// let myCreateDate = new Date("01-14-2024");
+
+// console.log(myCreateDate.toLocaleString())
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now() / 1000));
+
+
+let newdate = new Date()
+console.log(newdate.getMonth() + 1)
+console.log(newdate.getDay())
+console.log(newdate.getTime())
+
+// console.log(`${newdate.getDay()} and the time is${newdate.getTime()}`)
+
